3d4768135d88e4d94d560c78df50b3b8da287a23
Angela Lynn <angela.lynn@traveloka.com>
2021-07-18 21:20:27 +0700

    badges frontend

--- app/src/main/java/com/bx001/levelupsociety/BadgeActivity.java
+++ app/src/main/java/com/bx001/levelupsociety/BadgeActivity.java
@@ -1,14 +1,29 @@
 package com.bx001.levelupsociety;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class BadgeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_badge);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_badge);
+  }
+
+  public void viewProgressionMap(View view) {
+//    Intent intent = new Intent(this, )
+//    ToDo
+  }
+
+  public void getBadge(View view) {
+//    ToDo
+  }
+
+  public void goBack(View view) {
+    this.finish();
+  }
 }
