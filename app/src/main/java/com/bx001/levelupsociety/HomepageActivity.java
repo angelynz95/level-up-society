88cee4edfffe1dc9c2993ec49b21783acf7c576d
Angela Lynn <angela.lynn@traveloka.com>
2021-07-18 15:18:16 +0700

    homepage banner frontend

--- app/src/main/java/com/bx001/levelupsociety/HomepageActivity.java
+++ app/src/main/java/com/bx001/levelupsociety/HomepageActivity.java
@@ -3,7 +3,10 @@ package com.bx001.levelupsociety;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 
 import com.bx001.levelupsociety.api.Api;
 import com.bx001.levelupsociety.api.ApiImpl;
