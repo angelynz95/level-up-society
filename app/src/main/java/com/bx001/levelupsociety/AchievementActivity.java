e2acad32cc82515b2936a1bd984137c60eca9c53
Angela Lynn <angela.lynn@traveloka.com>
2021-07-18 20:56:19 +0700

    get badge frontend

--- app/src/main/java/com/bx001/levelupsociety/AchievementActivity.java
+++ app/src/main/java/com/bx001/levelupsociety/AchievementActivity.java
@@ -1,7 +1,9 @@
 package com.bx001.levelupsociety;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AchievementActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AchievementActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_achievement);
+  }
+
+  public void goBack(View view) {
+    Intent intent = new Intent(this, HomepageActivity.class);
+    this.startActivity(intent);
   }
 }
