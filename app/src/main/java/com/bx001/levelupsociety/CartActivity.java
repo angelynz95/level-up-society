896a0913a8d08ba03322dc7e808b6bd5e6a0ff1f
Angela Lynn <angela.lynn@traveloka.com>
2021-07-18 20:28:29 +0700

    cart frontend

--- app/src/main/java/com/bx001/levelupsociety/CartActivity.java
+++ app/src/main/java/com/bx001/levelupsociety/CartActivity.java
@@ -1,5 +1,6 @@
 package com.bx001.levelupsociety;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,11 @@ public class CartActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_cart);
+  }
+
+  public void pay(View view) {
+    Intent intent = new Intent(this, AchievementActivity.class);
+    this.startActivity(intent);
   }
 
   public void goBack(View view) {
